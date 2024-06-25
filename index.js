509605474be62208c36959dd3dcd502ad3a06c29
AlexanderGardiner <misc@alexandergardiner.com>
2024-06-25 12:27:27 -0700

    Update to public path

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.static("public"));
+const path = require("path");
+app.use("/platformerv2", express.static(path.join(__dirname, "public")));
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
